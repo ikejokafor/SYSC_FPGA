d71f575bc2b775aadda37add79e8de3b3ea47031
Ikenna Okafor <ikennajeshuaokafor@gmail.com>
2020-04-01 13:48:28 -0400

    Updates

--- inc/SYSC_FPGA.hpp
+++ inc/SYSC_FPGA.hpp
@@ -23,7 +23,12 @@ SC_MODULE(SYSC_FPGA)
         cnn_layer_accel = new CNN_Layer_Accel("CNN_Layer_Accel");
         cnn_layer_accel->clk(clk);
         m_sysc_fpga_hndl = new SYSC_FPGA_hndl();
-	    m_sysc_fpga_hndl->hardware_init();
+        if(m_sysc_fpga_hndl->hardware_init() == -1)
+        {
+            std::cout << "Hardware Init Failed" << std::endl;
+            sc_core::sc_stop();
+            exit(1);
+        }
 	    SC_THREAD(main)
             sensitive << clk.posedge_event();
     }
--- src/SYSC_FPGA.cpp
+++ src/SYSC_FPGA.cpp
@@ -13,26 +13,47 @@ void SYSC_FPGA::main()
 	uint64_t addr;
 	while(true)
 	{
+		// Config
 		wait();
 		addr = m_sysc_fpga_hndl->waitConfig();
 		wait();
 		cnn_layer_accel->setMemory(addr);
 
 		// Image
-		wait();
-		addr = m_sysc_fpga_hndl->waitParam();
-		wait();
-		cnn_layer_accel->setMemory(addr);
-		// Kernels
-		wait();
-		addr = m_sysc_fpga_hndl->waitParam();
-		wait();
-		cnn_layer_accel->setMemory(addr);
-		// Output
-		wait();
-		addr = m_sysc_fpga_hndl->waitParam();
-		wait();
-		cnn_layer_accel->setMemory(addr);
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
+		//
+		// // Kernels 3x3
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
+		//
+		// // Kernels 1x1
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
+		//
+		// // PartialMaps
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
+		//
+		// // ResidualMaps
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
+		//
+		// // Output
+		// wait();
+		// addr = m_sysc_fpga_hndl->waitParam();
+		// wait();
+		// cnn_layer_accel->setMemory(addr);
 
 		wait();
 		m_sysc_fpga_hndl->waitStart();
